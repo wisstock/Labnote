b439594cd614ade52440d40fb54a7d4e7a2447fd
wisstock <omnia.fatum@gmail.com>
2024-09-17 01:46:36 +0300

    17.09.2024: FRET analysis update

--- projects/PhD/6_2021_NMDAR_plasticity/exp/2024_08_29_AP2_FRET_60|60|180_stat/stat_fret.R
+++ projects/PhD/6_2021_NMDAR_plasticity/exp/2024_08_29_AP2_FRET_60|60|180_stat/stat_fret.R
@@ -274,11 +274,12 @@ calc.mixmdl <- function(input_vector) {
   comp2 <- dnorm(x = input_dens$x,
                  mean = mixmdl$mu[2],
                  sd = mixmdl$sigma[2]) * mixmdl$lambda[2]
-  return(data.frame(val = input_dens$x,
+  return(list(data.frame(val = input_dens$x,
                     raw = input_dens$y,
                     comp1 = comp1,
                     comp2 = comp2,
-                    comp_comb = comp1+comp2))
+                    comp_comb = comp1+comp2),
+              mixmdl))
 }
 
 calc.mixmdl3 <- function(input_vector) {
@@ -304,8 +305,9 @@ calc.mixmdl3 <- function(input_vector) {
 selected.mask <- 'fret'
 comp.scale.factor <- 50
 b.width <- 0.003
-pre.indexes <- c(0,1,2,3,4,5,6)
-post.indexes <- c(12,13,14,15)
+base.indexes <- c(0,1,2,3,4,5,6)
+mid.indexes <- c(12,13,14,15)
+end.indexes <- c(26,27,28)
 
 # selected frames
 df.fret.plot <- df.mask %>%
@@ -315,26 +317,27 @@ df.fret.plot <- df.mask %>%
   mutate(roi = as.factor(roi),
          cell_id = id) %>%
   droplevels() %>%
-  unite('roi_id', id:roi, sep = '-')
+  unite('roi_id', id:roi, sep = '-') %>%
+  select(roi_id, int, index, time, mask, cell_id)
 
-p1 <- ggplot(data = df.fret.plot,
+ggplot(data = df.fret.plot,
        aes(x = index, y = int, color = roi_id, group = roi_id)) +
   stat_summary(fun = median,
                geom = 'line', size = .5) +
   stat_summary(fun = median,
                geom = 'point', size = 1) +
   stat_summary(data = df.fret.plot,
-               aes(x = index, y = int, group = channel),
+               aes(x = index, y = int, group = cell_id),
                color = 'black',
                fun = median,
                geom = 'point', size = 0.75) +
   stat_summary(data = df.fret.plot,
-               aes(x = index, y = int, group = channel),
+               aes(x = index, y = int, group = cell_id),
                color = 'black',
                fun = median,
                geom = 'line', size = 0.3) +
   stat_summary(data = df.fret.plot,
-               aes(x = index, y = int, group = channel),
+               aes(x = index, y = int, group = cell_id),
                color = 'black',
                fun.min = function(z) { quantile(z,0.25) },
                fun.max = function(z) { quantile(z,0.75) },
@@ -343,11 +346,15 @@ p1 <- ggplot(data = df.fret.plot,
   annotate('rect', xmin = 6, xmax = 12, ymin = -Inf, ymax = Inf,
            alpha = 0.2, fill = 'red') +
   annotate('rect',
-           xmin = pre.indexes[1], xmax = rev(pre.indexes)[1],
+           xmin = base.indexes[1], xmax = rev(base.indexes)[1],
            ymin = -Inf, ymax = Inf,
            alpha = 0.075, color = 'black') +
   annotate('rect',
-           xmin = post.indexes[1], xmax = rev(post.indexes)[1],
+           xmin = mid.indexes[1], xmax = rev(mid.indexes)[1],
+           ymin = -Inf, ymax = Inf,
+           alpha = 0.075, color = 'black') +
+  annotate('rect',
+           xmin = end.indexes[1], xmax = rev(end.indexes)[1],
            ymin = -Inf, ymax = Inf,
            alpha = 0.075, color = 'black') +
   labs(title = 'FRET profiles in individual ROIs',
@@ -357,15 +364,15 @@ p1 <- ggplot(data = df.fret.plot,
   scale_fill_manual(values = rainbow(length(df.fret.plot$roi_id))) +
   theme_classic() +
   theme(legend.position="none") +
-  facet_wrap(~id)
+  facet_wrap(~cell_id)
 
-ggsave('fret_prof.png', p1, dpi = 300)
+# ggsave('fret_prof.png', p1, dpi = 300)
 
 
-# pre
-df.fret.pre <- df.mask %>%
+# base
+df.fret.base <- df.mask %>%
   filter(channel == 'Eapp',
-         index %in% pre.indexes) %>%
+         index %in% base.indexes) %>%
   select(id, roi, int, mask) %>%
   droplevels() %>%
   group_by(id, roi, mask) %>%
@@ -376,43 +383,47 @@ df.fret.pre <- df.mask %>%
   distinct() %>%
   unite('roi_id', id:roi, sep = '-')
 
-df.pre_mixmdl <- calc.mixmdl(df.fret.pre$int[df.fret.pre$mask == selected.mask]) 
+bm <- calc.mixmdl(df.fret.base$int[df.fret.base$mask == selected.mask]) 
+df.base_mixmdl <- bm[[1]]
+base_mixmgl <- bm[[2]]
+remove(bm)
 # ggplot(data = df.pre_mixmdl, aes(x = val)) +
 #   geom_line(aes(y = comp1), color = 'red', alpha = .5) +
 #   geom_line(aes(y = comp2), color = 'blue', alpha = .5) +
 #   geom_line(aes(y = comp_comb), lty = 2) +
 #   geom_line(aes(y = raw))
 
-p2 <- ggplot() +
-  geom_dotplot(data = df.fret.pre %>% filter(mask == selected.mask),
+plot.base <- ggplot() +
+  geom_dotplot(data = df.fret.base %>% filter(mask == selected.mask),
                aes(x = int, fill = roi_id),
                stackgroups = TRUE, binwidth = b.width,
                binpositions = "all", method = "histodot") +
-  geom_line(data = df.pre_mixmdl,
+  geom_line(data = df.base_mixmdl,
             aes(x = val, y = comp1 / comp.scale.factor),
             color = 'red') +
-  geom_line(data = df.pre_mixmdl,
+  geom_line(data = df.base_mixmdl,
             aes(x = val, y = comp2 / comp.scale.factor),
             color = 'blue') +
-  geom_line(data = df.pre_mixmdl,
+  geom_line(data = df.base_mixmdl,
             aes(x = val, y = comp_comb / comp.scale.factor),
             lty = 2) +
-  scale_fill_manual(values = rainbow(length(df.fret.post$roi_id[df.fret.post$mask == selected.mask]))) +
+  geom_vline(xintercept = base_mixmgl$mu, lty = 3) +
+  scale_fill_manual(values = rainbow(length(df.fret.base$roi_id[df.fret.base$mask == selected.mask]))) +
   theme_classic() +
   theme(legend.position="none",
         axis.text.y=element_blank(), 
         axis.ticks.y=element_blank(),
         axis.title.y = element_blank()) +
-  labs(title = 'Pre app. average FRET in individual ROIs',
+  labs(title = 'Base app. average FRET in individual ROIs',
        x = expression(E[app])) +
-  xlim(-0.005,0.13)
+  xlim(-0.005,0.11)
 
-ggsave('fret_pre.png', p2, dpi = 300)
+# ggsave('fret_pre.png', p2, dpi = 300)
 
-# post
-df.fret.post <- df.mask %>%
+# mid
+df.fret.mid <- df.mask %>%
   filter(channel == 'Eapp',
-         index %in% post.indexes) %>%
+         index %in% mid.indexes) %>%
   select(id, roi, int, mask) %>%
   droplevels() %>%
   group_by(id, roi, mask) %>%
@@ -423,74 +434,183 @@ df.fret.post <- df.mask %>%
   distinct() %>%
   unite('roi_id', id:roi, sep = '-')
 
-df.post_mixmdl <- calc.mixmdl(df.fret.post$int[df.fret.post$mask == selected.mask]) 
+mm <- calc.mixmdl(df.fret.mid$int[df.fret.mid$mask == selected.mask]) 
+df.mid_mixmdl <- mm[[1]]
+mid_mixmgl <- mm[[2]]
+remove(mm)
 
-p3 <- ggplot() +
-  geom_dotplot(data = df.fret.post %>% filter(mask == selected.mask),
+plot.mid <- ggplot() +
+  geom_dotplot(data = df.fret.mid %>% filter(mask == selected.mask),
                aes(x = int, fill = roi_id),
                stackgroups = TRUE, binwidth = b.width,
                binpositions = "all", method = "histodot") +
-  geom_line(data = df.post_mixmdl,
+  geom_line(data = df.mid_mixmdl,
             aes(x = val, y = comp1 / comp.scale.factor),
             color = 'red') +
-  geom_line(data = df.post_mixmdl,
+  geom_line(data = df.mid_mixmdl,
             aes(x = val, y = comp2 / comp.scale.factor),
             color = 'blue') +
-  geom_line(data = df.post_mixmdl,
+  geom_line(data = df.mid_mixmdl,
             aes(x = val, y = comp_comb / comp.scale.factor),
             lty = 2) +
-  scale_fill_manual(values = rainbow(length(df.fret.post$roi_id[df.fret.post$mask == selected.mask]))) +
+  geom_vline(xintercept = mid_mixmgl$mu, lty = 3) +
+  scale_fill_manual(values = rainbow(length(df.fret.mid$roi_id[df.fret.mid$mask == selected.mask]))) +
   theme_classic() +
   theme(legend.position="none",
         axis.text.y=element_blank(), 
         axis.ticks.y=element_blank(),
         axis.title.y = element_blank()) +
-  labs(title = 'Post app. average FRET in individual ROIs',
+  labs(title = 'Mid app. average FRET in individual ROIs',
        x = expression(E[app])) +
-  xlim(-0.005,0.13)
+  xlim(-0.005,0.11)
 
-ggsave('fret_post.png', p3, dpi = 300)
+# ggsave('fret_post.png', p3, dpi = 300)
+
+
+# end
+df.fret.end <- df.mask %>%
+  filter(channel == 'Eapp',
+         index %in% end.indexes) %>%
+  select(id, roi, int, mask) %>%
+  droplevels() %>%
+  group_by(id, roi, mask) %>%
+  mutate(int = mean(int),
+         roi = as.factor(roi),
+         cell_id = id) %>%
+  ungroup() %>%
+  distinct() %>%
+  unite('roi_id', id:roi, sep = '-')
+
+em <- calc.mixmdl(df.fret.end$int[df.fret.end$mask == selected.mask]) 
+df.end_mixmdl <- em[[1]]
+end_mixmgl <- em[[2]]
+remove(em)
+
+plot.end <- ggplot() +
+  geom_dotplot(data = df.fret.end %>% filter(mask == selected.mask),
+               aes(x = int, fill = roi_id),
+               stackgroups = TRUE, binwidth = b.width,
+               binpositions = "all", method = "histodot") +
+  geom_line(data = df.end_mixmdl,
+            aes(x = val, y = comp1 / comp.scale.factor),
+            color = 'red') +
+  geom_line(data = df.end_mixmdl,
+            aes(x = val, y = comp2 / comp.scale.factor),
+            color = 'blue') +
+  geom_line(data = df.end_mixmdl,
+            aes(x = val, y = comp_comb / comp.scale.factor),
+            lty = 2) +
+  geom_vline(xintercept = end_mixmgl$mu, lty = 3) +
+  scale_fill_manual(values = rainbow(length(df.fret.end$roi_id[df.fret.end$mask == selected.mask]))) +
+  theme_classic() +
+  theme(legend.position="none",
+        axis.text.y=element_blank(), 
+        axis.ticks.y=element_blank(),
+        axis.title.y = element_blank()) +
+  labs(title = 'End app. average FRET in individual ROIs',
+       x = expression(E[app])) +
+  xlim(-0.005,0.11)
+
+
+# combined plot
+plot_grid(plot.base, plot.mid, plot.end,
+          ncol = 1)  # labels = "AUTO",
 
 
 ##### TIMEPOINTS BOX #####
-df.to.box <- df.mask %>%
-  filter(index %in% c(2, 9, 13, 25),
-         channel == 'Eapp',
-         dist_group != 'min',
-         id != '24_05_16_04') %>%
+sites.threshold <- 0.03
+
+df.fret.sites <- df.mask %>%
+  filter(channel == 'Eapp',
+         int_val == 'abs',
+         mask == 'fret') %>%
+  mutate(roi = as.factor(roi),
+         cell_id = id) %>%
   droplevels() %>%
-  group_by(id, channel, index, mask) %>%
-  mutate(int_med = median(int), int_iqr = IQR(int), idx = as.factor(index)) %>%
+  unite('roi_id', id:roi, sep = '-') %>%
+  group_by(roi_id) %>%
+  mutate(wash_tail_int = mean(int[index %in% seq(16,28)]),
+         site_type = as.factor(ifelse(wash_tail_int > sites.threshold, 'spine', 'shaft'))) %>%
   ungroup() %>%
-  select(-time, -roi, -int, -dist, -dist_group, -channel, -index) %>%
-  distinct()
+  select(-time, -dist, -dist_group, -int_val, -channel, -mask)
+
+
+
+
+# df.to.box <- df.fret.sites  %>%  # by time points
+#   filter(index %in% c(3, 15, 24)) %>%
+#   droplevels() %>%
+#   select(-roi_id, -wash_tail_int) %>%
+#   mutate(index = as.factor(index)) %>%
+#   group_by(cell_id, index, site_type) %>%
+#   mutate(int_med = median(int), int_iqr = IQR(int)) %>%
+#   select(-int) %>%
+#   ungroup() %>%
+#   distinct()
+
+df.to.box <- df.fret.sites  %>%  # by time points
+ filter(index %in% c(3, 15, 24)) %>%
+ droplevels() %>%
+ select(-roi_id, -wash_tail_int) %>%
+ mutate(index = as.factor(index)) %>%
+ group_by(cell_id, index, site_type) %>%
+ mutate(int_med = median(int), int_iqr = IQR(int)) %>%
+ select(-int) %>%
+ ungroup() %>%
+ distinct()
+
+
+# IS ZERO
+wilcox.test(df.to.box$int_med[df.to.box$index == 3 & df.to.box$site_type == 'spine'], mu = 0)
+wilcox.test(df.to.box$int_med[df.to.box$index == 3 & df.to.box$site_type == 'shaft'], mu = 0)
+
+df.base.zero.stat <- df.to.box %>%
+  filter(index == 3) %>%
+  group_by(site_type) %>%
+  wilcox_test(int_med~1, mu = 0) %>%
+  add_significance() %>%
+  mutate(y.position = c(0.045,0.02), group2 = c(0,0))
+
+ggplot(df.to.box %>% filter(index == 3),
+       aes(y = int_med)) +
+  geom_boxplot() +
+  stat_pvalue_manual(df.base.zero.stat, label = 'p.signif',
+                     hide.ns = TRUE, remove.bracket = TRUE, label.size = 7) +
+  geom_hline(yintercept = 0, lty = 2) +
+  theme_classic() +
+  theme(axis.text.x = element_blank(), 
+        axis.ticks.x = element_blank(),
+        axis.title.x = element_blank()) +
+  labs(y = expression(E[app])) +
+  facet_wrap(~site_type)
+  
+
 
 # I vs T
 df.to.box.stat.it <- df.to.box %>%
-  select(-id, -int_iqr) %>%
-  group_by(mask) %>%
-  pairwise_wilcox_test(int_med ~ idx, p.adjust.method = "BH") %>%
-  add_xy_position(fun = "mean_sd")  
+  select(-cell_id, -int_iqr) %>%
+  group_by(site_type) %>%
+  pairwise_wilcox_test(int_med ~ index, p.adjust.method = "BH") %>%
+  add_xy_position(fun = "mean_sd")
 
 ggplot(data = df.to.box,  #  %>% filter(mask == 'fret')
-       aes(x = idx, y = int_med)) +
-  geom_boxplot(aes(fill = idx)) +
+       aes(x = index, y = int_med)) +
+  geom_boxplot(aes(fill = index)) +
   stat_pvalue_manual(df.to.box.stat.it, label = 'p.adj.signif',
                      hide.ns = TRUE) +
-  facet_wrap(facets = vars(mask), ncol = 2) 
+  facet_wrap(~site_type, ncol = 2) 
 
 # I vs M
 df.to.box.stat.im <- df.to.box %>%
-  select(-id, -int_iqr) %>%
-  group_by(idx) %>%
-  wilcox_test(int_med ~ mask) %>%
+  select(-cell_id, -int_iqr) %>%
+  group_by(index) %>%
+  wilcox_test(int_med ~ site_type) %>%
   add_significance() %>%
   add_xy_position(fun = "mean_sd")  
 
 ggplot(data = df.to.box,
-       aes(x = mask, y = int_med)) +
-  geom_boxplot(aes(fill = mask)) +
+       aes(x = site_type, y = int_med)) +
+  geom_boxplot(aes(fill = site_type)) +
   stat_pvalue_manual(df.to.box.stat.im, label = 'p.signif',
                      hide.ns = TRUE) +
-  facet_wrap(facets = vars(idx), ncol = 4)
-
+  facet_wrap(~index, ncol = 4)
