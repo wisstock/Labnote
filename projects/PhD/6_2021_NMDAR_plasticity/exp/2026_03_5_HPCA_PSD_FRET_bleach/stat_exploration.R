0588b0af6fc9ea7b6f295a2de6fc5f866d807d50
wisstock <omnia.fatum@gmail.com>
2026-06-27 15:52:59 +0300

    27.06.2026

--- projects/PhD/6_2021_NMDAR_plasticity/exp/2026_03_5_HPCA_PSD_FRET_bleach/stat_exploration.R
+++ projects/PhD/6_2021_NMDAR_plasticity/exp/2026_03_5_HPCA_PSD_FRET_bleach/stat_exploration.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(tidyr)
 library(purrr)
 library(rstatix)
+library(glue)
 library(gridExtra)
 library(ggplot2)
 library(ggpubr)
@@ -11,6 +12,7 @@ library(ggforce)
 library(cowplot)
 library(ggsci)
 library(introdataviz)
+library(ggmagnify)
 
 setwd('/home/wisstock/bio_note/projects/PhD/6_2021_NMDAR_plasticity/exp/2026_03_5_HPCA_PSD_FRET_bleach')
 
@@ -41,27 +43,27 @@ df.fret <- df.full %>%
 
 remove(df.full, df.full.id.summary)
 
-df.bg.raw <- read.csv('df_snr_test.csv')
-
-df.bg.mean <- df.bg.raw %>%
-  select(ch0_mean, ch1_mean, ch3_mean) %>%
-  mutate(index = seq(1,120)) %>%
-  rename_at(.vars = vars(ends_with("_mean")),
-            .funs = funs(sub("[_]mean$", "", .))) %>%
-  pivot_longer(cols = 1:3, names_to = "ch", values_to = "val") %>%
-  mutate(ch = as.factor(ch), wtf = as.factor('mean'))
-  
-df.bg.sd <- df.bg.raw %>%
-  select(ch0_sd, ch1_sd, ch3_sd) %>%
-  mutate(index = seq(1,120)) %>%
-  rename_at(.vars = vars(ends_with("_sd")),
-            .funs = funs(sub("[_]sd$", "", .))) %>%
-  pivot_longer(cols = 1:3, names_to = "ch", values_to = "val") %>%
-  mutate(ch = as.factor(ch), wtf = as.factor('sd'))
-
-df.bg <- rbind(df.bg.mean, df.bg.sd)
-
-remove(df.bg.raw, df.bg.mean, df.bg.sd)
+# df.bg.raw <- read.csv('df_snr_test.csv')
+# 
+# df.bg.mean <- df.bg.raw %>%
+#   select(ch0_mean, ch1_mean, ch3_mean) %>%
+#   mutate(index = seq(1,120)) %>%
+#   rename_at(.vars = vars(ends_with("_mean")),
+#             .funs = funs(sub("[_]mean$", "", .))) %>%
+#   pivot_longer(cols = 1:3, names_to = "ch", values_to = "val") %>%
+#   mutate(ch = as.factor(ch), wtf = as.factor('mean'))
+#   
+# df.bg.sd <- df.bg.raw %>%
+#   select(ch0_sd, ch1_sd, ch3_sd) %>%
+#   mutate(index = seq(1,120)) %>%
+#   rename_at(.vars = vars(ends_with("_sd")),
+#             .funs = funs(sub("[_]sd$", "", .))) %>%
+#   pivot_longer(cols = 1:3, names_to = "ch", values_to = "val") %>%
+#   mutate(ch = as.factor(ch), wtf = as.factor('sd'))
+# 
+# df.bg <- rbind(df.bg.mean, df.bg.sd)
+# 
+# remove(df.bg.raw, df.bg.mean, df.bg.sd)
 
 ##### PLOT PARAMETERS #####
 start_box <- seq(0, 1)
@@ -79,10 +81,8 @@ ea.color <- 'coral2'
 
 
 ##### PANNEL 2 PROFILES INT #####
-
-
 # abs
-ggplot(data = df.ch,
+plot.abs <- ggplot(data = df.ch,
        aes(x = index, y = abs_int, colour = ch, fill = ch)) +
   geom_hline(yintercept = 0, linetype = 2) +
   stat_summary(fun = median,
@@ -109,7 +109,7 @@ ggplot(data = df.ch,
                       labels = c('DD', 'DA', 'AA'),
                       name = 'Channel') +
   theme_classic() +
-  theme(legend.position = c(0.9,0.8),
+  theme(legend.position = c(0.4,0.8),
         text=element_text(size = font.size, family = font.fam),
         plot.caption = element_text(size = font.size-4),
         legend.title = element_text(size = font.size-3)) +
@@ -117,38 +117,8 @@ ggplot(data = df.ch,
        y = 'Intensity, a.u.',
        caption = 'n = 1/2/155 (cultures/cells/ROIs)')
 
-# SNR
-ch0.bg.val <- df.bg$val[df.bg$ch == 'ch0' & df.bg$wtf == 'mean']
-ch0.bg.var <- df.bg$val[df.bg$ch == 'ch0' & df.bg$wtf == 'sd']
-ch1.bg.val <- df.bg$val[df.bg$ch == 'ch1' & df.bg$wtf == 'mean']
-ch1.bg.var <- df.bg$val[df.bg$ch == 'ch1' & df.bg$wtf == 'sd']
-ch3.bg.val <- df.bg$val[df.bg$ch == 'ch3' & df.bg$wtf == 'mean']
-ch3.bg.var <- df.bg$val[df.bg$ch == 'ch3' & df.bg$wtf == 'sd']
-
-df.snr <- df.ch %>%
-  select(ch, index, abs_int, id) %>%
-  group_by(ch, index, id) %>%
-  mutate(val = mean(abs_int)) %>%
-  select(-abs_int) %>%
-  distinct() %>%
-  ungroup() %>%
-  group_by(ch, index) %>%
-  mutate(val = mean(val)) %>%
-  select(-id) %>%
-  distinct() %>%
-  ungroup() %>%
-  mutate(snr = if_else())
-
-ggplot(data = df.bg %>% filter(wtf == 'mean'),
-       aes(x = index, y = val, color = ch)) +
-  geom_line()
-
-ggplot(data = df.snr, 
-       aes(x = index, y = val, color = ch)) +
-  geom_line()
-
 # dF
-ggplot(data = df.ch,
+plot.df <- ggplot(data = df.ch,
        aes(x = index, y = dF0_int, colour = ch, fill = ch)) +
   geom_hline(yintercept = 0, linetype = 2) +
   stat_summary(fun = median,
@@ -166,19 +136,114 @@ ggplot(data = df.ch,
                       labels = c('DD', 'DA', 'AA'),
                       name = 'Channel') +
   theme_classic() +
-  theme(legend.position = c(0.9,0.8),
+  theme(legend.position = 'none',
+        text=element_text(size = font.size, family = font.fam),
+        plot.caption = element_text(size = font.size-4)) +
+  labs(x = 'Time, s',
+       y = expression(ΔF/F[0]))
+
+# fin plot
+plot.abs
+plot.df
+
+plot.int <- ggdraw(plot.abs) +
+  draw_plot(plot.df, x = 0.52, y = 0.58, width = 0.45, height = 0.4)
+
+plot.int
+save_plot('plot_int.png', plot.int, base_width = 9.2, base_height = 4)
+
+
+# # SNR
+# ch0.bg.val <- df.bg$val[df.bg$ch == 'ch0' & df.bg$wtf == 'mean']
+# ch0.bg.var <- df.bg$val[df.bg$ch == 'ch0' & df.bg$wtf == 'sd']
+# ch1.bg.val <- df.bg$val[df.bg$ch == 'ch1' & df.bg$wtf == 'mean']
+# ch1.bg.var <- df.bg$val[df.bg$ch == 'ch1' & df.bg$wtf == 'sd']
+# ch3.bg.val <- df.bg$val[df.bg$ch == 'ch3' & df.bg$wtf == 'mean']
+# ch3.bg.var <- df.bg$val[df.bg$ch == 'ch3' & df.bg$wtf == 'sd']
+# 
+# df.cnr <- df.ch %>%
+#   select(ch, index, abs_int, id) %>%
+#   group_by(ch, index, id) %>%
+#   mutate(val = mean(abs_int)) %>%
+#   select(-abs_int) %>%
+#   distinct() %>%
+#   ungroup() %>%
+#   group_by(ch, index) %>%
+#   mutate(val = mean(val)) %>%
+#   select(-id) %>%
+#   distinct() %>%
+#   ungroup() %>%
+#   mutate(cnr = case_when(ch == 'ch0' ~ (val - ch0.bg.val)/ch0.bg.var,
+#                          ch == 'ch1' ~ (val - ch1.bg.val)/ch0.bg.var,
+#                          ch == 'ch3' ~ (val - ch3.bg.val)/ch0.bg.var,
+#                          .default = 0))
+# 
+# ggplot(data = df.bg %>% filter(wtf == 'mean'),
+#        aes(x = index, y = val, color = ch)) +
+#   geom_line()
+# 
+# ggplot(data = df.cnr, 
+#        aes(x = index, y = val, color = ch)) +
+#   geom_line()
+
+
+##### PANNEL 2 BOXPLOT #####
+# raw
+df.ch.box <- df.ch %>%
+  mutate(box = as.factor(case_when(index %in% start_box ~ 'Start',
+                                   index %in% end_box ~ 'End',
+                                   .default = 'x'))) %>%
+  filter(box != 'x') %>%
+  droplevels() %>%
+  group_by(ch, box, roi_id) %>%
+  mutate(box = factor(box, levels = c('Start', 'End'), ordered = TRUE),
+         med_int = median(abs_int),
+         med_dF = median(dF_int),
+         med_dF0 = median(dF0_int)) %>%
+  ungroup() %>%
+  select(ch, box, med_int, med_dF, med_dF0, roi_id) %>%
+  distinct()
+
+length(levels(df.ch.box$roi_id))
+
+
+df.ch.box.abs.stat <- df.ch.box %>%
+  group_by(ch) %>%
+  wilcox_test(med_int ~ box) %>%
+  add_significance() %>%
+  add_xy_position()
+
+
+box.int <- ggplot(data = df.ch.box,
+       aes(x = box, y = med_int)) +
+  geom_boxplot(aes(fill = ch), alpha = box.alpha) +
+  geom_hline(yintercept = 0, linetype = 2) +
+  stat_summary(aes(group = ch),
+               fun = median,
+               geom = 'point', size = 1) +
+  stat_summary(aes(group = ch),
+               fun = median,
+               geom = 'line', size = 0.5) +
+  stat_pvalue_manual(df.ch.box.abs.stat, hide.ns = TRUE) +
+  facet_wrap(~ch, scale = 'free',
+             labeller = as_labeller(c("ch0" = "DD", "ch1" = "DA", "ch3" = "AA"))) +
+  theme_classic() +
+  theme(legend.position = 'none',
         text=element_text(size = font.size, family = font.fam),
         plot.caption = element_text(size = font.size-4),
         legend.title = element_text(size = font.size-3)) +
-  labs(x = 'Time, s',
-       y = 'Intensity, a.u.',
-       caption = 'n = 1/2/155 (cultures/cells/ROIs)')
+  scale_fill_manual(values = c('ch0' = ch0.color, 'ch1' = ch1.color, 'ch3' = ch3.color)) +
+  labs(caption = 'n = 1/2/155 (cultures/cells/ROIs)',
+       x = '',
+       y = 'Intensity, a.u.')
 
+box.int
+save_plot('plot_box_int.png', box.int, base_width = 4.8, base_height = 4)
 
 ##### PANNEL 2 PROFILES FRET #####
 Ea_scale = 25
 
-ggplot(data = df.fret %>% mutate(abs_int = if_else(ch == "Ea", abs_int * Ea_scale, abs_int)),
+plot.fret <- ggplot(data = df.fret %>% mutate(abs_int = if_else(ch == "Ea", abs_int * Ea_scale, abs_int)),
        aes(x = index, y = abs_int, colour = ch, fill = ch)) +  #  %>% filter(ch == 'Ea')
   geom_hline(yintercept = 0, linetype = 2) +
   stat_summary(fun = median,
@@ -191,69 +256,105 @@ ggplot(data = df.fret %>% mutate(abs_int = if_else(ch == "Ea", abs_int * Ea_scal
                geom = 'ribbon', linewidth = 0, alpha = .15) +
   scale_y_continuous(expression(F[c]), 
                      sec.axis = sec_axis(~ . / Ea_scale, name = expression(E[D]))) +
-  scale_fill_manual(values = c('Fc' = fc.color, 'Ea' = ea.color),
-                    labels = c(expression(F[c]), expression(E[D])),
+  scale_fill_manual(values = c('Fc' = fc.color, 'Ea' = ea.color),  # 
+                    labels = c(expression(E[D]), expression(F[c])),
                     name = 'FRET') +
   scale_colour_manual(values = c('Fc' = fc.color, 'Ea' = ea.color),
-                      labels = c(expression(F[c]), expression(E[D])),
+                      labels = c(expression(E[D]), expression(F[c])),
                       name = 'FRET') +
   theme_classic() +
-  theme(legend.position = c(0.1,0.25),
+  theme(legend.position = c(0.1,0.5),
         text=element_text(size = font.size, family = font.fam),
         plot.caption = element_text(size = font.size-4),
         legend.title = element_text(size = font.size-3)) +
   labs(caption = 'n = 1/2/155 (cultures/cells/ROIs)',
        x = 'Time, s')  # y = expression(ΔF/F[0])
 
+plot.fret
+save_plot('plot_fret.png', plot.fret, base_width = 6.4, base_height = 4)
 
-##### PANNEL 2 BOXPLOT #####
-# raw
-df.ch.box <- df.ch %>%
+###### FRET PANNEL 2 PDF PLOT #####
+df.fret.box <- df.fret %>%
   mutate(box = as.factor(case_when(index %in% start_box ~ 'Start',
-                         index %in% end_box ~ 'End',
-                         .default = 'x'))) %>%
+                                   index %in% end_box ~ 'End',
+                                   .default = 'x'))) %>%
   filter(box != 'x') %>%
   droplevels() %>%
   group_by(ch, box, roi_id) %>%
   mutate(box = factor(box, levels = c('Start', 'End'), ordered = TRUE),
          med_int = median(abs_int),
          med_dF = median(dF_int),
-         med_dF0 = median(dF0_int)) %>%
+         med_dF0 = median(dF0_int),
+         ch_box = interaction(ch, box)) %>%
   ungroup() %>%
-  select(ch, box, med_int, med_dF, med_dF0, roi_id) %>%
+  select(ch, box, ch_box, med_int, med_dF, med_dF0, roi_id) %>%
   distinct()
+
+# Ea
+ea.from <- c(xmin = 0.58, xmax = 1.42, ymin = -0.001, ymax = 0.03)
+ea.to <- c(1.1, 1.5, 0.2, 0.4)
   
-length(levels(df.ch.box$roi_id))
-
-
-df.ch.box.abs.stat <- df.ch.box %>%
-  group_by(ch) %>%
-  wilcox_test(med_int ~ box) %>%
-  add_significance() %>%
-  add_xy_position()
-
-
-ggplot(data = df.ch.box,
-       aes(x = box, y = med_int)) +
-  geom_boxplot(aes(fill = ch), alpha = box.alpha) +
-  stat_summary(aes(group = ch),
+plot.pdf.fret <- ggplot(data = df.fret.box %>% filter(ch == 'Ea'),
+       aes(x = ch, y = med_int, group=box, fill = ch_box)) +
+  geom_hline(yintercept = 0, linetype = 2) +
+  stat_summary(aes(group = ch_box, color = ch_box),
                fun = median,
-               geom = 'point', size = 1) +
-  stat_summary(aes(group = ch),
-               fun = median,
-               geom = 'line', size = 0.5) +
-  stat_pvalue_manual(df.ch.box.abs.stat) +
-  facet_wrap(~ch, scale = 'free',
-             labeller = as_labeller(c("ch0" = "DD", "ch1" = "DA", "ch3" = "AA"))) +
+               geom = 'point', size = 3) +
+  geom_split_violin(alpha = box.alpha) +
+  scale_fill_manual(values = c('Ea.Start' = ea.color, 'Ea.End' = 'coral4'),
+                    labels = c('Start', 'End'),
+                    name = '') +
+  scale_color_manual(values = c('Ea.Start' = ea.color, 'Ea.End' = 'coral4'),
+                     labels = c('Start', 'End'),
+                     name = '') +
   theme_classic() +
-  theme(legend.position = 'none',
+  theme(legend.position = c(0.2,0.8),
         text=element_text(size = font.size, family = font.fam),
         plot.caption = element_text(size = font.size-4),
-        legend.title = element_text(size = font.size-3)) +
-  scale_fill_manual(values = c('ch0' = ch0.color, 'ch1' = ch1.color, 'ch3' = ch3.color)) +
-  labs(caption = 'n = 1/2/155 (cultures/cells/ROIs)',
-       x = '',
-       y = 'Intensity, a.u.')
+        legend.title = element_text(size = font.size-3),
+        axis.title.x = element_blank(),
+        axis.text.x  = element_blank(),
+        axis.ticks.x = element_blank()) +
+  geom_magnify(from = ea.from, to = ea.to,
+               axes = "y") +
+  labs(y = expression(E[D]))
+
+plot.pdf.fret
+save_plot('plot_pdf_fret.png', plot.pdf.fret, base_width = 4, base_height = 4)
+
+# Fc
+fc.from <- c(xmin = 0.58, xmax = 1.42, ymin = -0.001, ymax = 1.5)
+fc.to <- c(1.1, 1.5, 2.5, 7.5)
+
+plot.pdf.fc <- ggplot(data = df.fret.box %>% filter(ch == 'Fc'),
+       aes(x = ch, y = med_int, group=box, fill = ch_box)) +
+  geom_hline(yintercept = 0, linetype = 2) +
+  stat_summary(aes(group = ch_box, color = ch_box),
+               fun = median,
+               geom = 'point', size = 3) +
+  geom_split_violin(alpha = box.alpha) +
+  scale_fill_manual(values = c('Fc.Start' = fc.color, 'Fc.End' = 'skyblue4'),
+                    labels = c('Start', 'End'),
+                    name = '') +
+  scale_color_manual(values = c('Fc.Start' = fc.color, 'Fc.End' = 'skyblue4'),
+                     labels = c('Start', 'End'),
+                     name = '') +
+  theme_classic() +
+  theme(legend.position = c(0.2,0.8),
+        text=element_text(size = font.size, family = font.fam),
+        plot.caption = element_text(size = font.size-4),
+        legend.title = element_text(size = font.size-3),
+        axis.title.x = element_blank(),
+        axis.text.x  = element_blank(),
+        axis.ticks.x = element_blank()) +
+  geom_magnify(from = fc.from, to = fc.to,
+               axes = "y") +
+  labs(y = expression(F[c]))
+
+plot.pdf.fc
+save_plot('plot_pdf_fc.png', plot.pdf.fc, base_width = 4, base_height = 4)
+
+
 
 ###### FRET DEVIATION TEST #####
 df.fret.box <- df.fret %>%
